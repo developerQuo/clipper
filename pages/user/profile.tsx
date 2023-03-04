1a323bfcb2f98bc79556993ac662f1ce57819821
dev-wiillus <dev@wiillus.com>
2023-03-04 12:51:55 +0900

    added profile page

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1,8 +1,36 @@
 import { GetServerSideProps } from "next";
+import { Session } from "next-auth";
+import Image from "next/image";
 import serverSideAuthGuard from "../../components/utils/serverSideAuthGuard";
 
-export default function Profile() {
-	return <>profile</>;
+type InputProps = {
+	session: Session;
+};
+
+export default function Profile({ session }: InputProps) {
+	console.log(session);
+	const {
+		user: { name, email, image },
+	} = session;
+	return (
+		<>
+			<h1 className="text-center text-3xl font-bold">User Profile</h1>
+			<div className="mt-12 flex flex-col items-center space-y-4 text-xl font-semibold">
+				<div className="avatar">
+					<div className="w-24 rounded-full">
+						<Image
+							alt="profile-image"
+							src={image ?? ""}
+							width={64}
+							height={64}
+						/>
+					</div>
+				</div>
+				<div>{name}</div>
+				<div>{email}</div>
+			</div>
+		</>
+	);
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
