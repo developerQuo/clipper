3c680775822e508c22bff6fa517ec7dd6292cf53
robin-wiillus <robin@wiillus.com>
2024-04-13 16:53:55 +0900

    test list of bookmark button

---
+++ components/ui/bookmark/button.test.ts
@@ -0,0 +1,11 @@
+import { unmountComponentAtNode } from 'react-dom';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('bookmark > button', () => {
+	test('change color on click');
+
+	test('submit data on click');
+
+	test('change color along with prop');
+});
